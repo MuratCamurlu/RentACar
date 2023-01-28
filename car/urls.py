146636f774eeaf8e5a613d1456fe934267c7b592
MuratÇamurlu <mcamurlu1923@gmail.com>
2023-01-28 09:24:28 +0300

    first commit

---
+++ car/urls.py
@@ -0,0 +1,12 @@
+
+from django.urls import path
+from rest_framework import routers
+from .views import CarView
+
+router=routers.DefaultRouter()
+router.register('car',CarView)
+
+urlpatterns = [
+    
+]
+urlpatterns+=router.urls
